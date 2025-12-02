7e5ccc90d7018ce99a7eb555d2f5080a2c2f9ac4
zorua162 <55893520+Zorua162@users.noreply.github.com>
2025-12-02 20:51:10 +0000

    Create day3 from template

---
+++ day3/src/main.rs
@@ -0,0 +1,69 @@
+use std::fs;
+
+#[derive(Debug, PartialEq)]
+struct Answer {
+    answer: usize
+}
+
+trait InputGetter {
+    fn get_input(&self) -> String;
+    
+}
+
+struct LocalFileInputGetter {
+    path: &'static str,
+}
+
+impl InputGetter for LocalFileInputGetter {
+    fn get_input(&self) -> String {
+        return fs::read_to_string(self.path).expect("Input file is expected");
+    }
+}
+
+fn part1(contents: &String) -> Option<Answer> {
+    println!("Contents is {contents}");
+    None
+
+}
+
+// Attempted answers
+
+fn part2(contents: &String) -> Option<Answer> {
+    println!("Contents is {contents}");
+    None
+
+}
+
+// Attempted answers
+
+fn main() {
+
+    let contents = LocalFileInputGetter{ path: "input.txt"}.get_input();
+    let result1 = part1(&contents);
+    println!("Part1 result {result1:?}");
+
+    let result2 = part2(&contents);
+    println!("Part2 result {result2:?}");
+
+}
+
+
+// Tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+    let contents = LocalFileInputGetter{ path: "input.txt"}.get_input();
+        let result = part1(&contents);
+        assert_eq!(result, Some(Answer { answer: 2081}));
+    }
+
+    #[test]
+    fn test_part2() {
+    let contents = LocalFileInputGetter{ path: "input.txt"}.get_input();
+        let result = part2(&contents);
+        assert_eq!(result, Some(Answer { answer: 2341}));
+    }
+}
