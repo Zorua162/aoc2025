8e484e918565d8337edfe6cbdaafe718511a6007
zorua162 <55893520+Zorua162@users.noreply.github.com>
2025-12-03 07:39:41 +0000

    WIP day3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -20,9 +20,45 @@ impl InputGetter for LocalFileInputGetter {
     }
 }
 
+fn find_highest_and_pos(line: &str) -> (usize, usize) {
+
+    let mut highest = 0;
+    let mut highest_loc = 0;
+
+    for (i, char) in line.chars().enumerate() {
+
+        let digit = char.to_digit(10).expect("Expected a digit here, got {char}");
+        if digit > highest {
+            highest = digit;
+            highest_loc = i;
+        }
+    }
+
+    (highest.try_into().expect("Couldn't convert to usize"), highest_loc)
+}
+
+fn exactly_two_highest(line: &str) -> usize {
+
+    let (highest, highest_location) = find_highest_and_pos(line);
+    
+    // Run find_highest_and_pos on a slice from the given highest location
+    let (second_highest, _) = find_highest_and_pos(&line[(highest_location+1)..]);
+
+    println!("{highest} x {second_highest} ");
+
+    highest * second_highest
+}
+
 fn part1(contents: &String) -> Option<Answer> {
     println!("Contents is {contents}");
-    None
+
+    let mut answer: usize = 0;
+
+    for line in contents.lines() {
+        answer += exactly_two_highest(line)
+    }
+
+    Some(Answer { answer })
 
 }
 
@@ -53,17 +89,41 @@ fn main() {
 mod tests {
     use super::*;
 
+
+    #[ignore]
     #[test]
     fn test_part1() {
-    let contents = LocalFileInputGetter{ path: "input.txt"}.get_input();
+        let contents = LocalFileInputGetter{ path: "input.txt"}.get_input();
         let result = part1(&contents);
         assert_eq!(result, Some(Answer { answer: 2081}));
     }
 
     #[test]
+    fn test_part1_example() {
+        let contents = "987654321111111\n\
+                              811111111111119\n\
+                              234234234234278\n\
+                              818181911112111".to_string();
+        let result = part1(&contents);
+        assert_eq!(result, Some(Answer { answer: 357}));
+    }
+
+    #[ignore]
+    #[test]
     fn test_part2() {
-    let contents = LocalFileInputGetter{ path: "input.txt"}.get_input();
+        let contents = LocalFileInputGetter{ path: "input.txt"}.get_input();
         let result = part2(&contents);
         assert_eq!(result, Some(Answer { answer: 2341}));
+    }
+
+    #[ignore]
+    #[test]
+    fn test_part2_example() {
+        let contents = "987654321111111\n\
+                              811111111111119\n\
+                              234234234234278\n\
+                              818181911112111".to_string();
+        let result = part1(&contents);
+        assert_eq!(result, Some(Answer { answer: 357}));
     }
 }
