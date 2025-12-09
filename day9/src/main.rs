3d1670e222c32d9e11ae50aea2773d75b5cd4c9b
zorua162 <55893520+Zorua162@users.noreply.github.com>
2025-12-09 21:58:37 +0000

    (feat) Completed day9 part1

---
+++ day9/src/main.rs
@@ -0,0 +1,194 @@
+use std::{cmp::Ordering, fs};
+
+#[derive(Debug, PartialEq)]
+struct Answer {
+    answer: u64,
+}
+
+trait InputGetter {
+    fn get_input(&self) -> String;
+}
+
+struct LocalFileInputGetter {
+    path: &'static str,
+}
+
+impl InputGetter for LocalFileInputGetter {
+    fn get_input(&self) -> String {
+        return fs::read_to_string(self.path).expect("Input file is expected");
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Clone, Hash)]
+struct TwoDimensionalLocation {
+    x: i64,
+    y: i64,
+}
+
+#[derive(Eq, PartialEq, Clone)]
+struct TwoDimensionalLocationPair {
+    loc1: TwoDimensionalLocation,
+    loc2: TwoDimensionalLocation,
+}
+
+impl TwoDimensionalLocationPair {
+    fn calculate_square_size(&self) -> i64 {
+        let x_diff: i64 = self.loc1.x - (self.loc2.x + 1).abs();
+        let y_diff: i64 = self.loc1.y - (self.loc2.y + 1).abs();
+        return x_diff * y_diff;
+    }
+}
+
+impl PartialOrd for TwoDimensionalLocationPair {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        (self.calculate_square_size()).partial_cmp(&other.calculate_square_size())
+    }
+}
+
+impl Ord for TwoDimensionalLocationPair {
+    fn cmp(&self, other: &Self) -> Ordering {
+        (self.calculate_square_size())
+            .partial_cmp(&other.calculate_square_size())
+            .expect("Expected an Ordering here")
+    }
+}
+
+fn parse_locations(contents: &String) -> Vec<TwoDimensionalLocation> {
+    let mut locations: Vec<TwoDimensionalLocation> = vec![];
+
+    for line in contents.lines() {
+        let split: Vec<&str> = line.split(",").collect();
+        locations.push(TwoDimensionalLocation {
+            x: split[0].parse().expect("Expected a value here"),
+            y: split[1].parse().expect("Expected a value here"),
+        })
+    }
+
+    return locations;
+}
+
+fn create_pairs(locations: Vec<TwoDimensionalLocation>) -> Vec<TwoDimensionalLocationPair> {
+    let mut out_pairs: Vec<TwoDimensionalLocationPair> = vec![];
+    for location in &locations {
+        for other_location in &locations {
+            if location == other_location {
+                continue;
+            }
+
+            out_pairs.push(TwoDimensionalLocationPair {
+                loc1: location.clone(),
+                loc2: other_location.clone(),
+            })
+        }
+    }
+
+    return out_pairs;
+}
+
+fn part1(contents: &String) -> Option<Answer> {
+    let locations: Vec<TwoDimensionalLocation> = parse_locations(contents);
+
+    let mut pairs: Vec<TwoDimensionalLocationPair> = create_pairs(locations);
+
+    pairs.sort();
+
+    let answer = pairs[pairs.len()-1].calculate_square_size() as u64;
+
+    return Some(Answer { answer })
+}
+
+// Part 1 attempted answers
+
+fn generate_map(contents: &str) -> Vec<Vec<bool>> {
+    // True in the map means that there is a red or green tile there
+    // Everywhere else defaults to false
+
+    // First draw the shape
+
+    // Then flood fill it
+    
+    todo!()
+}
+
+fn part2(contents: &String) -> Option<Answer> {
+    // First generate map of all "in" and "out" positions
+    let map:  Vec<Vec<bool>> = generate_map(contents);
+    
+    println!("Contents is {contents}");
+    None
+}
+
+
+// Part 2 attempted answers
+
+fn main() {
+    let contents = LocalFileInputGetter { path: "input.txt" }.get_input();
+    let do_part1 = true;
+    let do_part2 = false;
+    if do_part1 {
+        let result1 = part1(&contents);
+        println!("Part1 result {result1:?}");
+    }
+
+    if do_part2 {
+        let result2 = part2(&contents);
+        println!("Part2 result {result2:?}");
+    }
+}
+
+// Tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct Setup {
+        contents: String,
+    }
+
+    impl Setup {
+        fn new() -> Self {
+            Self {
+                contents: "7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3"
+                .to_string(),
+            }
+        }
+    }
+
+    #[test]
+    fn test_part1_example() {
+        let setup = Setup::new();
+        let contents = &setup.contents;
+        let result = part1(&contents);
+        assert_eq!(result, Some(Answer { answer: 50 }));
+    }
+
+    #[test]
+    fn test_part1() {
+        let contents = LocalFileInputGetter { path: "input.txt" }.get_input();
+        let result = part1(&contents);
+        assert_eq!(result, Some(Answer { answer: 4777816465 }));
+    }
+
+    #[test]
+    fn test_part2_example() {
+        let setup = Setup::new();
+        let contents = &setup.contents;
+        let result = part2(&contents);
+        assert_eq!(result, Some(Answer { answer: 24 }));
+    }
+
+    #[ignore]
+    #[test]
+    fn test_part2() {
+        let contents = LocalFileInputGetter { path: "input.txt" }.get_input();
+        let result = part2(&contents);
+        assert_eq!(result, Some(Answer { answer: 2341 }));
+    }
+}
